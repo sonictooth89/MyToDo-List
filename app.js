0d9658807be29e104289aa8af61f475c1990278b
Malgorzata Nowak <nowak.em@interia.eu>
2022-11-19 16:04:00 +0100

    js edited

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 
 const newElement = () => {
+    
     const li = document.createElement('li');
     const inputValue = document.getElementById('myInput').value;
     const node = document.createTextNode(inputValue);
     li.appendChild(node);
-
-
     
     if (inputValue === '') {
         alert('You must written something :)')
     } else {
+    
         const element = document.getElementById('myUl');
         element.appendChild(li);
     }
