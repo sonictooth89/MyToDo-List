708dc2c716fe7284720e5b5c151ce5e32a737b42
Malgorzata Nowak <nowak.em@interia.eu>
2022-11-26 19:32:37 +0100

    object in localstorage saved

--- app.js
+++ app.js
@@ -1,61 +1,43 @@
-// odczytanie wszystkich elementow z lokal storage
+const entries = Object.entries(localStorage);
+
 
 if (localStorage != null) {
-    const entries = Object.entries(localStorage);
     for (const entry of entries) {
         const [key, value] = entry;
-        
+        const objValue = JSON.parse(value).value;
+        const objChecked = JSON.parse(value).checked;
         const li = document.createElement('li');
         li.setAttribute('class', 'list-group-item');
         li.innerHTML= `
-                <input class="form-check-input me-1" type="checkbox" value="">
-                <label class="form-check-label">${value}</label>`
+                <input class="form-check-input me-1" type="checkbox" value="" ${objChecked}>
+                <label class="form-check-label">${objValue}</label>`
         const element = document.getElementById('myUl');
         element.appendChild(li);
+        
     }
-}
+};
 
 
 const newElement = () => {
-    const id = String(Math.floor(Math.random()*999999999999999999))+'abcvpooodsaubcvlop';
+    const id = String(Math.floor(Math.random()*999999999999999999))+'abcvpoood';
     const inputValue = document.getElementById('myInput').value;
+    const objValue = {value: inputValue, checked: true};
+    
     const li = document.createElement('li');
     li.setAttribute('class', 'list-group-item');
 
     li.innerHTML= `
-                <input class="form-check-input me-1" type="checkbox" value="">
-                <label class="form-check-label">${inputValue}</label>`
+                <input class="form-check-input me-1" type="checkbox" value="" id="${id}">
+                <label class="form-check-label">${objValue.value}</label>`
     
     if (inputValue === '') {
         alert('You must written something :)')
     } else {
         const element = document.getElementById('myUl');
         element.appendChild(li);
-        localStorage.setItem(id, inputValue);
+        localStorage.setItem(id, JSON.stringify(objValue));
     }
 
     document.getElementById("myInput").value = "";
 };
-
-// const setPriority = () => {
-//     const prioHeigh = document.getElementById('prioHeigh');
-//     const prioMedium = document.getElementById('prioMedium');
-//     const prioLow = document.getElementById('prioLow');
-
-//     if (prioHeigh) {
-//         prioHeigh.setAttribute('checked', '');
-//     } else if (prioMedium) {
-//         prioMedium.setAttribute('checked', '');
-//     } else {
-//         prioLow.setAttribute('checked', '');
-//     }
-// }
-
-
-// const showMessage = () => {
-//     const para = document.getElementById('message');
-//     para.innerHTML = '<h1 style="color: #f63f">Your message is here :)</h1>'
-// }
-
-
 
